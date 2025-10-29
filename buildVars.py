adf92f6aaae58463635eea6bd0d8cf6f59eb48a0
mrido <ridhokurniawan7390@gmail.com>
2025-10-29 12:50:35 +0700

    modified build file

--- buildVars.py
+++ buildVars.py
@@ -29,15 +29,15 @@ addon_info = {
 	# Author(s)
 	"addon_author": "mrido1",
 	# URL for the add-on documentation support
-	"addon_url": "https://github.com/wendrillaksenow/pausingInformation",
+	"addon_url": "https://github.com/starkrush123/SpeechPacer",
 	# URL for the add-on repository where the source code can be found
-	"addon_sourceURL": "https://github.com/wendrillaksenow/pausingInformation",
+	"addon_sourceURL": "https://github.com/starkrush123/SpeechPacer",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": 2019.3,
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": 2025.1,
+	"addon_lastTestedNVDAVersion": 2025.3,
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
